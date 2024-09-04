de28f9b0b393ef4696a97789d9c11d2a481af8cb
Logan Schmillen <schmillen20@gmail.com>
2024-09-03 20:40:19 -0500

    first working version, simple print statement

--- src/main/java/com/lschmi/quizzapp/QuestionController.java
+++ src/main/java/com/lschmi/quizzapp/QuestionController.java
@@ -1,13 +1,15 @@
 package com.lschmi.quizzapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("question")
+@RestController
+@RequestMapping("question")
 public class QuestionController {
     @GetMapping("allQuestions")
     public String getAllQuestions() {
         return "Hi, these are your questions";
     }
-    
 }
+
