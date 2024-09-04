87c84bfb1d24e11ce8ee487f0f0053537b8e9a2f
Logan Schmillen <schmillen20@gmail.com>
2024-09-04 15:38:12 -0500

    created DAO package and have started getallquestions method. still issues

--- src/main/java/com/lschmi/quizzapp/controller/QuestionController.java
+++ src/main/java/com/lschmi/quizzapp/controller/QuestionController.java
@@ -1,14 +1,24 @@
 package com.lschmi.quizzapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lschmi.quizzapp.Question;
+import com.lschmi.quizzapp.service.QuestionService;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions() {
+    public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 }
--- src/main/java/com/lschmi/quizzapp/service/QuestionService.java
+++ src/main/java/com/lschmi/quizzapp/service/QuestionService.java
@@ -1,5 +1,16 @@
 package com.lschmi.quizzapp.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.lschmi.quizzapp.Question;
+
+@Service
 public class QuestionService {
+
+    public List<Question> getAllQuestions() {
+
+    }
     
 }
