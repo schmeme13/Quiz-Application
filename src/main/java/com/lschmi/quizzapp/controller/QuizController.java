d6d3f490713de87d19485dbd833d6dd729df5509
Logan Schmillen <schmillen20@gmail.com>
2024-09-09 22:36:10 -0500

    can create quizzes with the available data in the DB. Wrapper class made for user view of the questions the answer is hidden

--- src/main/java/com/lschmi/quizzapp/controller/QuizController.java
+++ src/main/java/com/lschmi/quizzapp/controller/QuizController.java
@@ -1,12 +1,17 @@
 package com.lschmi.quizzapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lschmi.quizzapp.model.QuestionWrapper;
 import com.lschmi.quizzapp.service.QuizService;
 
 @RestController
@@ -20,5 +25,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestions(id);
     }
 }
--- src/main/java/com/lschmi/quizzapp/service/QuizService.java
+++ src/main/java/com/lschmi/quizzapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.lschmi.quizzapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lschmi.quizzapp.dao.QuestionDAO;
 import com.lschmi.quizzapp.dao.QuizDao;
 import com.lschmi.quizzapp.model.Question;
+import com.lschmi.quizzapp.model.QuestionWrapper;
 import com.lschmi.quizzapp.model.Quiz;
 
 @Service
@@ -41,5 +44,20 @@ public class QuizService {
         
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
+
+    
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionsFromDB) {
+            QuestionWrapper wrapper = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(wrapper);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+    
  
 }
