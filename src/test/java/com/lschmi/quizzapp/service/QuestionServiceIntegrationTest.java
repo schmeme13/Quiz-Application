be0cd221108d55e2a2cd262777664e56f37118c5
Logan Schmillen <schmillen20@gmail.com>
2025-03-05 21:24:26 -0600

    different test material

--- src/test/java/com/lschmi/quizzapp/service/QuestionServiceIntegrationTest.java
+++ src/test/java/com/lschmi/quizzapp/service/QuestionServiceIntegrationTest.java
@@ -16,21 +16,22 @@ public class QuestionServiceIntegrationTest {
 
     @Test
     public void testAddAndGetQuestionByCategory() {
-        // Given: Create a new question
+        //Given: Create a new question
         Question question = new Question();
-        question.setQuestionTitle("What is the capital of France?");
-        question.setOption1("Berlin");
-        question.setOption2("Madrid");
-        question.setOption3("Paris");
-        question.setOption4("Rome");
-        question.setRightAnswer("Paris");
+        question.setQuestionTitle("What is the primary purpose of Amazon S3?");
+        question.setOption1("Storing relational databases");
+        question.setOption2("Storing and retrieving any amount of data at any time");
+        question.setOption3("Running virtual machines");
+        question.setOption4("Creating serverless applications");
+        question.setRightAnswer("Storing and retrieving any amount of data at any time");
         question.setDifficulty_level("Easy");
-        question.setCategory("Geography");
+        question.setCategory("AWS S3");
 
-        // When: Add the question using the service
+
+        //When: Add the question using the service
         questionService.addQuestion(question);
 
-        // Then: Verify the question is saved by fetching it by category
+        //Then: Verify the question is saved by fetching it by category
         var response = questionService.getQuestionsByCategory("Geography");
 
         assertThat(response.getBody()).isNotNull();
